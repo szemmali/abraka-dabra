c71cd16131db097e70285cd9d9c3cffc717b2e92
Saddam ZEMMALI <saddam.zemmali@ooredoo.tn>
2018-04-15 20:14:08 +0100

    Update shell script to INSTALL AND CONFIGURE PostgreSQL 10 + phpPgAdmin on REDHAT and CentOS 7

--- Task-0-Install-PostgreSQL10.sh
+++ Task-0-Install-PostgreSQL10.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
-###### 	Date 15/11/2017
-######	Saddam ZEMMALI
-######	Project abraka-dabra 
-######	Task 	Install PostgreSQL 10 + phpPgAdmin
-
-echo "Version: 1.0 ¯\_(ツ)_/¯"
-echo "╔═══════════════════════════════════════╗"
-echo "║- Author: Saddam ZEMMALI               ║"
-echo "║- eMail:                               ║"
-echo "║- Version: 1.0                         ║"
-echo "╚═══════════════════════════════════════╝"
-
-
+##====================================================================================
+##		 Project:  abraka-dabra
+##        AUTHOR:  Eng. Saddam ZEMMALI
+##       CREATED:  15.04.2018 20:10:01
+##      REVISION:  ---
+##       Version:  1.0  ¯\_(ツ)_/¯
+##    Repository:  https://github.com/szemmali/abraka-dabra.git
+##	    	Task:  Install PostgreSQL 10 + phpPgAdmin
+##          FILE:  install_jenkins.sh
+##   Description:  This script will Install and Configure PostgreSQL 10 + phpPgAdmin
+##                 on RedHat/CentOS 7
+##   Requirement:  --
+##			Note:  -- 
+##          BUGS:  ---
+##====================================================================================
 echo "╔═══════════════════════════════════════════════════════╗"
 echo "║ 	   PostgreSQL  User Guide [10]                      ║"
 echo "╚═══════════════════════════════════════════════════════╝"
 
 echo "Enable PostgreSQL Yum Repository"
-yum -y localinstall https://yum.postgresql.org/10/redhat/rhel-7.5-x86_64/pgdg-redhat10-10-1.noarch.rpm
+sudo yum -y localinstall https://yum.postgresql.org/10/redhat/rhel-7.5-x86_64/pgdg-redhat10-10-1.noarch.rpm
 
 echo "Install PostgreSQL 10 Server"
-yum -y install postgresql10-server postgresql10
+sudo yum -y install postgresql10-server postgresql10
 
 echo "Initialize PGDATA"
-/usr/pgsql-10/bin/postgresql-10-setup initdb
+sudo /usr/pgsql-10/bin/postgresql-10-setup initdb
 
 echo "Start PostgreSQL Server"
-systemctl restart postgresql-10.service
-systemctl enable postgresql-10.service
+sudo systemctl restart postgresql-10.service
+sudo systemctl enable postgresql-10.service
 
 
 echo "Verify PostgreSQL Installation"
@@ -36,13 +38,13 @@ su - postgres -c "psql"
 echo "create password for user postgres for security purpose."
 #$ postgres=# \password postgres
 echo "Add New Rule to Firewalld"
-firewall-cmd --permanent --zone=public --add-port=5432/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=5432/tcp
 
 
 echo "╔═══════════════════════════════════════════════════════╗"
 echo "║ 	    phpPgAdmin User Guide [10]                      ║"
 echo "╚═══════════════════════════════════════════════════════╝"
-yum -y localinstall https://download.postgresql.org/pub/repos/yum/testing/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-2.noarch.rpm
+sudo yum -y localinstall https://download.postgresql.org/pub/repos/yum/testing/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-2.noarch.rpm
 
 echo "Install phpPgAdmin using Yum"
 yum install phpPgAdmin
